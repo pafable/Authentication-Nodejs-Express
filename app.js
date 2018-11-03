7353f00cedd529e51235a179969bbb1e847db7e9
pafable <39135798+pafable@users.noreply.github.com>
2018-11-03 16:40:08 -0400

    added secret page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect("mongodb://localhost/authDemo");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(require("express-session")({
     secret: "WTF Breh!!!",
@@ -35,11 +36,26 @@ app.get("/secret", function(req,res){
     res.render("secret");
 });
 
-//====================================
 //  Auth Routes
+
 //Show sign up form
 app.get("/register", function(req,res){
     res.render("register");
+});
+
+//handling user sign up
+app.post("/register", function(req,res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.render("/secret");
+        });
+    });
 });
 
 app.listen(80, process.env.IP, function(){
