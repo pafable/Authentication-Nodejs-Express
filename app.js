6ee597792c2b3d1c88aae207cea6cb3ff2baac06
syncleft <20546397+syncleft@users.noreply.github.com>
2018-11-03 11:38:54 -0400

    initial

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    localStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+
+mongoose.connect("mongodb://localhost/authDemo");
+
+var app = express();
+app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "WTF Breh!!!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/secret", function(req,res){
+    res.render("secret");
+});
+
+app.listen(80, process.env.IP, function(){
+    console.log("SERVER STARTED");
+});
